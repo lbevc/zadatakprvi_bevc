919144014a2f36e3939e508f02cff116639fd524
lbevc <bevc.lana@gmail.com>
2022-12-08 17:24:44 +0100

    Add project files.

---
+++ zadatakprvi_bevc/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace zadatakprvi_bevc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button_crvena = new System.Windows.Forms.Button();
+            this.button_narancasta = new System.Windows.Forms.Button();
+            this.button_zuta = new System.Windows.Forms.Button();
+            this.button_zelena = new System.Windows.Forms.Button();
+            this.button_plava = new System.Windows.Forms.Button();
+            this.button_ljubicasta = new System.Windows.Forms.Button();
+            this.button_crna = new System.Windows.Forms.Button();
+            this.button_bijela = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(280, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(299, 108);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Izaberite boju pozadine forme:";
+            // 
+            // button_crvena
+            // 
+            this.button_crvena.BackColor = System.Drawing.Color.Red;
+            this.button_crvena.ForeColor = System.Drawing.SystemColors.Window;
+            this.button_crvena.Location = new System.Drawing.Point(15, 34);
+            this.button_crvena.Name = "button_crvena";
+            this.button_crvena.Size = new System.Drawing.Size(92, 36);
+            this.button_crvena.TabIndex = 2;
+            this.button_crvena.Text = "crvena";
+            this.button_crvena.UseVisualStyleBackColor = false;
+            this.button_crvena.Click += new System.EventHandler(this.button_crvena_Click);
+            // 
+            // button_narancasta
+            // 
+            this.button_narancasta.BackColor = System.Drawing.Color.Orange;
+            this.button_narancasta.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.button_narancasta.Location = new System.Drawing.Point(158, 34);
+            this.button_narancasta.Name = "button_narancasta";
+            this.button_narancasta.Size = new System.Drawing.Size(96, 36);
+            this.button_narancasta.TabIndex = 3;
+            this.button_narancasta.Text = "narančasta";
+            this.button_narancasta.UseVisualStyleBackColor = false;
+            this.button_narancasta.Click += new System.EventHandler(this.button_narancasta_Click);
+            // 
+            // button_zuta
+            // 
+            this.button_zuta.BackColor = System.Drawing.Color.Yellow;
+            this.button_zuta.Location = new System.Drawing.Point(292, 34);
+            this.button_zuta.Name = "button_zuta";
+            this.button_zuta.Size = new System.Drawing.Size(93, 36);
+            this.button_zuta.TabIndex = 4;
+            this.button_zuta.Text = "žuta";
+            this.button_zuta.UseVisualStyleBackColor = false;
+            this.button_zuta.Click += new System.EventHandler(this.button_zuta_Click);
+            // 
+            // button_zelena
+            // 
+            this.button_zelena.BackColor = System.Drawing.Color.Green;
+            this.button_zelena.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.button_zelena.Location = new System.Drawing.Point(15, 106);
+            this.button_zelena.Name = "button_zelena";
+            this.button_zelena.Size = new System.Drawing.Size(92, 37);
+            this.button_zelena.TabIndex = 5;
+            this.button_zelena.Text = "zelena";
+            this.button_zelena.UseVisualStyleBackColor = false;
+            this.button_zelena.Click += new System.EventHandler(this.button_zelena_Click);
+            // 
+            // button_plava
+            // 
+            this.button_plava.BackColor = System.Drawing.Color.Blue;
+            this.button_plava.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.button_plava.Location = new System.Drawing.Point(158, 106);
+            this.button_plava.Name = "button_plava";
+            this.button_plava.Size = new System.Drawing.Size(96, 37);
+            this.button_plava.TabIndex = 6;
+            this.button_plava.Text = "plava";
+            this.button_plava.UseVisualStyleBackColor = false;
+            this.button_plava.Click += new System.EventHandler(this.button_plava_Click);
+            // 
+            // button_ljubicasta
+            // 
+            this.button_ljubicasta.BackColor = System.Drawing.Color.Purple;
+            this.button_ljubicasta.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.button_ljubicasta.Location = new System.Drawing.Point(292, 106);
+            this.button_ljubicasta.Name = "button_ljubicasta";
+            this.button_ljubicasta.Size = new System.Drawing.Size(93, 37);
+            this.button_ljubicasta.TabIndex = 7;
+            this.button_ljubicasta.Text = "ljubičasta";
+            this.button_ljubicasta.UseVisualStyleBackColor = false;
+            this.button_ljubicasta.Click += new System.EventHandler(this.button_ljubicasta_Click);
+            // 
+            // button_crna
+            // 
+            this.button_crna.BackColor = System.Drawing.Color.Black;
+            this.button_crna.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.button_crna.Location = new System.Drawing.Point(15, 176);
+            this.button_crna.Name = "button_crna";
+            this.button_crna.Size = new System.Drawing.Size(92, 37);
+            this.button_crna.TabIndex = 8;
+            this.button_crna.Text = "crna";
+            this.button_crna.UseVisualStyleBackColor = false;
+            this.button_crna.Click += new System.EventHandler(this.button_crna_Click);
+            // 
+            // button_bijela
+            // 
+            this.button_bijela.BackColor = System.Drawing.Color.White;
+            this.button_bijela.Location = new System.Drawing.Point(158, 174);
+            this.button_bijela.Name = "button_bijela";
+            this.button_bijela.Size = new System.Drawing.Size(96, 39);
+            this.button_bijela.TabIndex = 9;
+            this.button_bijela.Text = "bijela";
+            this.button_bijela.UseVisualStyleBackColor = false;
+            this.button_bijela.Click += new System.EventHandler(this.button_bijela_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button_bijela);
+            this.groupBox1.Controls.Add(this.button_crvena);
+            this.groupBox1.Controls.Add(this.button_narancasta);
+            this.groupBox1.Controls.Add(this.button_zuta);
+            this.groupBox1.Controls.Add(this.button_zelena);
+            this.groupBox1.Controls.Add(this.button_plava);
+            this.groupBox1.Controls.Add(this.button_ljubicasta);
+            this.groupBox1.Controls.Add(this.button_crna);
+            this.groupBox1.Location = new System.Drawing.Point(223, 174);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(415, 244);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "izbor boje";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Izbor Boja";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button_crvena;
+        private System.Windows.Forms.Button button_narancasta;
+        private System.Windows.Forms.Button button_zuta;
+        private System.Windows.Forms.Button button_zelena;
+        private System.Windows.Forms.Button button_plava;
+        private System.Windows.Forms.Button button_ljubicasta;
+        private System.Windows.Forms.Button button_crna;
+        private System.Windows.Forms.Button button_bijela;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ zadatakprvi_bevc/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace zadatakprvi_bevc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void button_crvena_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Red;
+        }
+
+        private void button_narancasta_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Orange;
+        }
+
+        private void button_zuta_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Yellow;
+        }
+
+        private void button_zelena_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Green;
+        }
+
+        private void button_plava_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Blue;
+        }
+
+        private void button_ljubicasta_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Purple;
+        }
+
+        private void button_crna_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Black;
+            label1.BackColor = Color.White;
+        }
+
+        private void button_bijela_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.White;
+        }
+    }
+}
